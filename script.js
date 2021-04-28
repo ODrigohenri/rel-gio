918d952bdc63f5f710e340194eef8cfbb385ddaa
Rodrigo Henrique Moreira <ct0036@blanver.com.br>
2021-04-28 09:23:06 -0300

    clean code add

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
-function carregar() {
+function load() {
           
-    var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('imagem')
-    var data = new Date()
-    var hora = data.getHours()
-    var minutos = data.getMinutes()
-    var segundos = data.getSeconds()
-   
-    msg.innerHTML = hora.toString().padStart(2, '0') + ":" + minutos.toString().padStart(2, '0') + ":" + segundos.toString().padStart(2, '0');
-     if(hora >= 0 && hora < 12) {
-        document.body.style.background = "url(https://cdn.pixabay.com/photo/2018/04/30/15/26/nature-3362956_960_720.jpg)";
-        document.body.style.backgroundSize = "cover";
-        document.body.style.backgroundRepeat = "no-repeat";
-        document.body.style.width =  "100vw";
-        document.body.style.height =  "100vh";
-        document.body.style.margin =  "0 auto";     
-     }else if (hora >= 12 && hora < 18) {
-        document.body.style.backgroundImage = "url(https://cdn.pixabay.com/photo/2013/04/04/12/34/mountains-100367_960_720.jpg)";
-        document.body.style.backgroundSize = "cover";
-        document.body.style.backgroundRepeat = "no-repeat";
-        document.body.style.width =  "100vw";
-        document.body.style.height =  "100vh";
-        document.body.style.margin =  "0 auto";     
-     }else{
-        document.body.style.background = "url(https://www.prefeitura.sp.gov.br/cidade/secretarias/upload/estrelas.jpg)";
-        document.body.style.backgroundSize = "cover";
-        document.body.style.backgroundRepeat = "no-repeat";
-        document.body.style.width =  "100vw";
-        document.body.style.height =  "100vh";
-        document.body.style.margin =  "0 auto";     
-     }
-     setTimeout('carregar()',500);
-   }
+   var msg = window.document.getElementById('msg')
+   var img = window.document.getElementById('imagem')
+   var data = new Date()
+   var hour = data.getHours()
+   var minute = data.getMinutes()
+   var second = data.getSeconds()
+  
+   msg.innerHTML = hour.toString().padStart(2, '0') + ":" + minute.toString().padStart(2, '0') + ":" + second.toString().padStart(2, '0');
+    if(hour >= 0 && hour < 12) {
+       document.body.style.background = "url(https://cdn.pixabay.com/photo/2018/04/30/15/26/nature-3362956_960_720.jpg)";
+       document.body.style.backgroundSize = "cover";
+       document.body.style.backgroundRepeat = "no-repeat";
+       document.body.style.width =  "100vw";
+       document.body.style.height =  "100vh";
+       document.body.style.margin =  "0 auto";     
+    }else if (hour >= 12 && hour < 18) {
+       document.body.style.backgroundImage = "url(https://cdn.pixabay.com/photo/2013/04/04/12/34/mountains-100367_960_720.jpg)";
+       document.body.style.backgroundSize = "cover";
+       document.body.style.backgroundRepeat = "no-repeat";
+       document.body.style.width =  "100vw";
+       document.body.style.height =  "100vh";
+       document.body.style.margin =  "0 auto";     
+    }else{
+       document.body.style.background = "url(https://www.prefeitura.sp.gov.br/cidade/secretarias/upload/estrelas.jpg)";
+       document.body.style.backgroundSize = "cover";
+       document.body.style.backgroundRepeat = "no-repeat";
+       document.body.style.width =  "100vw";
+       document.body.style.height =  "100vh";
+       document.body.style.margin =  "0 auto";     
+    }
+    setTimeout('load()',500);
+  }
